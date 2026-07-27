0bac3e518f15e7e456739a7d9a436a7e3ece2eff
ThomasPeruch <peruchthomas@gmail.com>
2026-07-27 16:06:54 -0300

    feedback login message in login.html

--- backend/src/main/java/com/peruch/fullstack_demo/controller/AuthController.java
+++ backend/src/main/java/com/peruch/fullstack_demo/controller/AuthController.java
@@ -3,13 +3,11 @@ package com.peruch.fullstack_demo.controller;
 import com.peruch.fullstack_demo.util.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     @PostMapping("/login")
--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { NgZone, Component, signal } from '@angular/core';
 import { AuthService } from '../auth';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -12,19 +12,22 @@ import { CommonModule } from '@angular/common';
 export class Login {
   username ='';
   password ='';
-  message ='';
+  message = signal('');
 
   constructor(private authService: AuthService) {}
 
   login() {
+    console.log(this.username),
+    console.log(this.password),
     this.authService.login(this.username, this.password)
       .subscribe({ 
         next: (res) => { 
           localStorage.setItem('token', res.token);
-          this.message = 'Login realizado com sucesso!';
+          console.log(res.token)
+          this.message.set('Login realizado com sucesso!');
         },
         error:() => {
-          this.message = "Credenciais inválidas"
+          this.message.set("Credenciais inválidas")
         }
       });
   }
