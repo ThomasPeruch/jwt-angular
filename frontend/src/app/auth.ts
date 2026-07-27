66436ea8ad9eb5cce91a97ce49ed39af5d38505b
ThomasPeruch <peruchthomas@gmail.com>
2026-07-27 15:16:29 -0300

    login html and logic implemented

--- frontend/src/app/auth.ts
+++ frontend/src/app/auth.ts
@@ -1,4 +1,13 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-@Service()
-export class Auth {}
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient){}
+
+    login(username: string, password: string) {
+        return this.http.post<{token: string}>(
+            'http://localhost:8080/auth/login',{username, password}
+        );
+    }
+}
--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  username ='';
+  password ='';
+  message ='';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.username, this.password)
+      .subscribe({ 
+        next: (res) => { 
+          localStorage.setItem('token', res.token);
+          this.message = 'Login realizado com sucesso!';
+        },
+        error:() => {
+          this.message = "Credenciais inválidas"
+        }
+      });
+  }
+}
