67e405fb955ca1fd650706b7beeb0a01213ad0ad
ThomasPeruch <peruchthomas@gmail.com>
2026-07-27 13:58:39 -0300

    backend adjusts; frontend initial commit

--- backend/src/main/java/com/peruch/fullstack_demo/security/SecurityConfig.java
+++ backend/src/main/java/com/peruch/fullstack_demo/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/auth/**").permitAll()
